9eb8b73df911d84a4a1d1593e03064edf76cc917
Jia Rong Gan <jiaronggan21@gmail.com>
2023-12-11 14:00:36 -0600

    new models and 2 view functions in app.py

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
 """Flask app for Cupcakes"""
+
+import os
+from flask import Flask, request, jsonify
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", 'postgresql:///cupcakes')
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+app.get("/api/cupcakes")
+def list_all_cupcakes():
+    """Return JSON of all cupcakes."""
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+app.get("/api/cupcakes/<cupcake_id>")
+def list_single_cupcake(cupcake_id):
+    """Return JSON {'cupcake' : {id, flavor, size, rating, image_url}}. """
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+
